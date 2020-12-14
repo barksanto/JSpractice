8d17a26ec2350c0ce9cfdd1da2429b7c10f13988
Barkley Santo <barkley.santo@gmail.com>
2020-12-14 14:07:32 -0600

    toggle play function working

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,36 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build our functions
+function togglePlay(){
+  if (video.paused){
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton(){
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip(){
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Hoop up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
