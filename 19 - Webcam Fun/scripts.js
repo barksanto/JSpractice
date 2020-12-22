7c20ecc19eee214a33e0e47974cccfacf9fdead0
Barkley Santo <barkley.santo@gmail.com>
2020-12-22 12:50:59 -0600

    take pixels out, modify them with redEffexct, put them back

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,7 +29,42 @@ function paintToCanvas (){
   //every 16ms - take image from webcam and put it into canvas
   setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height); // paint over top left corner @0,0 to the width variabeles we have
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    pixels = redEffect(pixels);
+    //put them back
+    ctx.putImageData(pixels, 0, 0);
+    
   }, 16)
 }
 
+function takePhoto(){
+  //played the sound
+  snap.currentTime = 0; // sets time to 0 so we can do it again and again
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src=${data} alt="handsome guy">`
+  strip.insertBefore(link, strip.firstChild);
+  console.log(data);
+}
+
+function redEffect(pixels){
+  for(let i = 0; i < pixels.length; i+=4){
+    pixels.data[i]  = pixels.data[i] + 100;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; 
+    pixels.data[i + 2] = pixels.data[i + 2] * .5;
+    // pixels[i + 3]//alpha
+  }
+  return pixels
+}
+
 getVideo();
+//once the video is playing listen for an event that emits
+// 'canplay', then run the paintToCanvas
+video.addEventListener('canplay', paintToCanvas); 
