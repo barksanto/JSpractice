9b872bec40ed473385a027453c8642268a179f08
Barkley Santo <barkley.santo@gmail.com>
2020-12-22 13:07:26 -0600

    added greenscreen effect

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -35,6 +35,7 @@ function paintToCanvas (){
     // pixels = redEffect(pixels); // UNCOMMENT THIS FOR RED
     // pixels = rgbSplit(pixels);  // UNCOMMENT THIS FOR RGBSPLIT
     //put them back
+    pixels = greenScreen(pixels);
     ctx.putImageData(pixels, 0, 0);
     
   }, 16)
@@ -74,6 +75,33 @@ function rgbSplit(pixels){
     // pixels[i + 3]//alpha
   }
   return pixels
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 
