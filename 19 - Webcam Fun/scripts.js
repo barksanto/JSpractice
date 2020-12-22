c887bf8087c59fa7e849fe80544d9554659f72d1
Barkley Santo <barkley.santo@gmail.com>
2020-12-22 11:34:21 -0600

    target video, create getVideo fn to request permission to turn on camera

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,14 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// get video being piped into our video element
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = localMediaStream; //this is an object right now, we need to convert it to a url
+})
+}
+
+getVideo();
