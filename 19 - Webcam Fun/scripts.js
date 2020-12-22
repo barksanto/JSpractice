a1810b42071033be4e3a99bcde1acb3584eea36a
Barkley Santo <barkley.santo@gmail.com>
2020-12-22 11:51:11 -0600

    setInterval to take img from video and paint it over canvas every 16ms

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -20,12 +20,16 @@ function paintToCanvas (){
   // get width and height of our video
   const width = video.videoWidth;
   const height = video.videoHeight;
-
+  
   //check width and height of our canvas
   //make canvas same size as video
   canvas.width = width;
   canvas.height = height;
   
-
-
+  //every 16ms - take image from webcam and put it into canvas
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height); // paint over top left corner @0,0 to the width variabeles we have
+  }, 16)
 }
+
+getVideo();
